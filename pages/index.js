5c625e15389cd93c5f20be52ef7f47f15709ad87
vipautomation86 <vipautomation86@gmail.com>
2023-05-17 05:04:21 +0530

    Update Readme file

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <div className="text-2xl bg-slate-300">Vip-Automation</div>
+  return <div className="text-2xl font-bold">Vip-Automation</div>
 }
