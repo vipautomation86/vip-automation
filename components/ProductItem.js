d514e67dad95e4fc0516ff08e5f0e8deec283a27
vipautomation86 <vipautomation86@gmail.com>
2023-05-19 00:30:28 +0530

    7. Create Product Details

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -2,22 +2,22 @@
 import Link from 'next/link'
 import React from 'react'
 
-export default function ProductItem({ product }) {
+export default function ProductItem({ product_ld }) {
   return (
     <div className="card">
-      <Link href={`/product/${product.slug}`}>
+      <Link href={`/product/${product_ld.slug}`}>
         <img
-          src={product.images}
-          alt={product.name}
+          src={product_ld.images}
+          alt={product_ld.name}
           className='="rounded shadow'
         />
       </Link>
       <div className="flex flex-col items-center justify-center p-5">
-        <Link href={`/product/${product.slug}`}>
-          <h2 className="text-lg">{product.name}</h2>
+        <Link href={`/product/${product_ld.slug}`}>
+          <h2 className="text-lg">{product_ld.name}</h2>
         </Link>
-        <p className="mb-2">{product.brand}</p>
-        <p>${product.price}</p>
+        <p className="mb-2">{product_ld.brand}</p>
+        <p>${product_ld.price}</p>
         <button className="primary-button" type="button">
           Add to Cart
         </button>
